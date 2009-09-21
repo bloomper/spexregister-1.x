c5486311ecad1be4e7a51ed4572d3b6b4cb7b7e0
Anders Jacobsson <anders.jacobsson@gmail.com>
2009-09-21 21:25:40 +0000

    Added session expiry

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,11 @@
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
-
+  expires_session :time => property(:session_length).minutes, :redirect_to => '/login'
+  
   # Scrub sensitive parameters from log
   filter_parameter_logging :password
-
+  
   before_filter :set_locale
   
   protected
@@ -16,10 +17,14 @@ class ApplicationController < ActionController::Base
     # if params[:locale] is nil then I18n.default_locale will be used
     I18n.locale = params[:locale]
   end
-
+  
   def default_url_options(options={})
     logger.debug "default_url_options is passed options: #{options.inspect}\n"
     { :locale => I18n.locale }
   end
-
+  
+  def on_expiry
+    flash[:notice] = I18n.t "views.base.session_expiry"
+  end
+  
 end
