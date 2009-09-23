57206f968770b7a86f2899427f86867bfb261111
Anders Jacobsson <anders.jacobsson@gmail.com>
2009-09-23 18:59:17 +0000

    Removed session_lifetime in favor of authlogic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,6 @@
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
-  expires_session :time => property(:session_length).minutes, :redirect_to => '/login'
   
   # Scrub sensitive parameters from log
   filter_parameter_logging :password
@@ -12,7 +11,6 @@ class ApplicationController < ActionController::Base
   before_filter :set_locale
   
   protected
-  
   def set_locale
     # if params[:locale] is nil then I18n.default_locale will be used
     I18n.locale = params[:locale]
@@ -21,10 +19,6 @@ class ApplicationController < ActionController::Base
   def default_url_options(options={})
     logger.debug "default_url_options is passed options: #{options.inspect}\n"
     { :locale => I18n.locale }
-  end
-  
-  def on_expiry
-    flash[:notice] = I18n.t "views.base.session_expiry"
   end
   
 end
