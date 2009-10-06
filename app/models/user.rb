96ee8af76180e9f9066f712b2ff92fa4a0b0194e
Anders Jacobsson <anders.jacobsson@gmail.com>
2009-10-06 21:31:29 +0000

    Fixed unit tests for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,17 @@
 class User < ActiveRecord::Base
   acts_as_authentic do |c|
-    c.logged_in_timeout = ApplicationConfig.session_length.minutes.to_i
+    c.logged_in_timeout = ApplicationConfig.logged_in_timeout
   end
   has_one :spexare, :dependent => :nullify
   has_and_belongs_to_many :user_groups
-  attr_protected :role
   
-  def deliver_password_reset_instructions!
-    reset_perishable_token!
-    # TODO: Implement me
-    #Notifier.deliver_password_reset_instructions(self)
+  def after_destroy
+    if User.count.zero?
+      raise I18n.t('user.cannot_delete_all_users')
+    end
   end
+  
+  validates_presence_of :spexare
+  validates_presence_of :user_groups
   
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,8 +1,74 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-
-  test "truth" do
+  
+  def setup
+    @password = Time.now.to_s
   end
-
+  
+  test "should be ok" do
+    user = User.create(:username => "test", :password => @password, :password_confirmation => @password, :user_groups => [ user_groups(:admins_user_group) ], :spexare => spexare(:spexare_1))
+    assert user.valid?, user.errors.full_messages.join("\n") 
+  end
+  
+  test "should not save user with no username" do
+    user = User.new(:username => nil, :password => @password, :password_confirmation => @password, :user_groups => [ user_groups(:admins_user_group) ], :spexare => spexare(:spexare_1))
+    assert(!user.valid?, "Should not save entry unless username has been set")
+    assert(user.errors.invalid?(:username), "Expected an error for missing username")
+  end
+  
+  test "should not save user with no password" do
+    user = User.new(:username => "test", :password => nil, :password_confirmation => nil, :user_groups => [ user_groups(:admins_user_group) ], :spexare => spexare(:spexare_1))
+    assert(!user.valid?, "Should not save entry unless password has been set")
+    assert(user.errors.invalid?(:password), "Expected an error for missing password")
+  end
+  
+  test "should not save user with no user groups" do
+    user = User.new(:username => "test", :password => @password, :password_confirmation => @password, :user_groups => [], :spexare => spexare(:spexare_1))
+    assert(!user.valid?, "Should not save entry unless user groups have been set")
+    assert(user.errors.invalid?(:user_groups), "Expected an error for missing user groups")
+  end
+  
+  test "should not save user with no spexare" do
+    user = User.new(:username => "test", :password => @password, :password_confirmation => @password, :user_groups => [ user_groups(:admins_user_group) ], :spexare => nil)
+    assert(!user.valid?, "Should not save entry unless spexare has been set")
+    assert(user.errors.invalid?(:spexare), "Expected an error for missing spexare")
+  end
+  
+  test "should not save user with invalid username" do
+    user = User.new(:username => "1", :password => @password, :password_confirmation => @password, :user_groups => [ user_groups(:admins_user_group) ], :spexare => spexare(:spexare_1))
+    assert(!user.valid?, "Should not save entry if an invalid username has been set")
+    assert(user.errors.invalid?(:username), "Expected an error for invalid username")
+    
+    user = User.new(:username => "%¤&¤%", :password => @password, :password_confirmation => @password, :user_groups => [ user_groups(:admins_user_group) ], :spexare => spexare(:spexare_1))
+    assert(!user.valid?, "Should not save entry if an invalid username has been set")
+    assert(user.errors.invalid?(:username), "Expected an error for invalid username")
+  end
+  
+  test "should not save user with invalid password" do
+    user = User.new(:username => "test", :password => "1", :password_confirmation => "1", :user_groups => [ user_groups(:admins_user_group) ], :spexare => spexare(:spexare_1))
+    assert(!user.valid?, "Should not save entry if an invalid password has been set")
+    assert(user.errors.invalid?(:password), "Expected an error for invalid password")
+    
+    user = User.new(:username => "test", :password => @password, :password_confirmation => "1", :user_groups => [ user_groups(:admins_user_group) ], :spexare => spexare(:spexare_1))
+    assert(!user.valid?, "Should not save entry if an invalid password has been set")
+    assert(user.errors.invalid?(:password), "Expected an error for invalid password")
+  end
+  
+  test "should not save user with already existing username" do
+    existing_user = users(:user)
+    user = User.new(:username => existing_user.username, :password => @password, :password_confirmation => @password, :user_groups => [ user_groups(:admins_user_group) ], :spexare => spexare(:spexare_1))
+    assert(!user.valid?, "Should not save entry if username already exists")
+    assert(user.errors.invalid?(:username), "Expected an error for duplicate username")
+  end
+  
+  test "should not be able to delete all users" do
+    begin
+      users(:user).destroy
+      users(:admin).destroy
+    rescue Exception
+      assert_equal I18n.t('user.cannot_delete_all_users'), $!.to_s
+    end
+  end
+  
 end
