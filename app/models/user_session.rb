107d3be31d56ab7a4cb1c178d5fb10c1593fe30d
Anders Jacobsson <anders.jacobsson@gmail.com>
2009-10-06 20:33:52 +0000

    Added more configuration possibilities

--- app/models/user_session.rb
+++ app/models/user_session.rb
@@ -1,2 +1,8 @@
 class UserSession < Authlogic::Session::Base
+  logout_on_timeout true
+  generalize_credentials_error_messages true
+  allow_http_basic_auth false
+  remember_me_for ApplicationConfig.remember_me_for
+  consecutive_failed_logins_limit ApplicationConfig.consecutive_failed_logins_limit
+  failed_login_ban_for ApplicationConfig.failed_login_ban_for
 end
