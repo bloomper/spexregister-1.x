56fa33e4c8b5d750b4ce931e5417a86d3ca807ad
Anders Jacobsson <anders.jacobsson@gmail.com>
2009-10-07 09:37:06 +0000

    Mistake

--- app/models/spexare.rb
+++ app/models/spexare.rb
@@ -6,7 +6,7 @@ class Spexare < ActiveRecord::Base
   acts_as_network :cohabitants, :join_table => :cohabitants, :foreign_key => 'spexare_id', :association_foreign_key => 'spexare_id_cohabitant'
   belongs_to :user
   has_attached_file :picture, :styles => { :thumb => ApplicationConfig.picture_thumbnail_size }
-  attr_protected :related_spexare, :picture_file_name, :picture_content_type, :picture_file_size
+  attr_protected :picture_file_name, :picture_content_type, :picture_file_size
   attr_encrypted :social_security_number, :key => 'A8AD3BC66E66FC6C255312D70FFA547E1CE8FB8A4382BE961DFFBED0DD45B340', :encode => true
   validates_as_email_address :email_address
 
