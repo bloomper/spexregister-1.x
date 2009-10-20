84ab187a8c8dbaece98ecb4b07d8e7a9a01bd212
Anders Jacobsson <anders.jacobsson@gmail.com>
2009-10-20 07:57:52 +0000

    Added named_scope for latest news

--- app/models/news.rb
+++ app/models/news.rb
@@ -1,4 +1,5 @@
 class News < ActiveRecord::Base
+  named_scope :latest, lambda { |*args| {:conditions => ["publication_date > ?", (args.first || 2.weeks.ago)]} }
 
   protected
   validates_presence_of :publication_date
