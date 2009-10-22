524eb9bbb5451abe50dae07a2fa38d4338194a4f
Anders Jacobsson <anders.jacobsson@gmail.com>
2009-10-22 08:23:19 +0000

    Fixed i18n of user_groups

--- app/helpers/user_groups_helper.rb
+++ app/helpers/user_groups_helper.rb
@@ -1,7 +1,9 @@
 module UserGroupsHelper
 
   def get_available_user_groups
-    UserGroup.to_dropdown
+   UserGroup.to_dropdown.each do |user_group|
+      user_group[0] = t("user_group.name.#{user_group[0].downcase}")
+    end
   end
 
 end
