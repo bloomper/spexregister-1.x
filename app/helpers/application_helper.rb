d6e2e14557f4c27adaf587a6bc1dd9e61fb20dfd
Anders Jacobsson <anders.jacobsson@gmail.com>
2009-10-23 18:05:26 +0000

    Need to evaluate JS failure response as well

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -83,7 +83,8 @@ module ApplicationHelper
           type: 'POST',
           url: '#{options[:url]}',
           data: ({_method: 'delete', authenticity_token: AUTH_TOKEN}),
-          success: function(r){ jQuery('##{dom_id resource}').fadeOut('hide'); eval(r);} 
+          success: function(r){ jQuery('##{dom_id resource}').fadeOut('hide'); eval(r); },
+          failure: function(r){ eval(r); } 
         });
       }
     });"
