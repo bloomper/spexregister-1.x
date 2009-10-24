3a5eb3d94c34eeceaf2971561001adf18a89f254
Anders Jacobsson <anders.jacobsson@gmail.com>
2009-10-24 19:15:25 +0000

    Fixed named_scope to not include future news items

--- app/models/news.rb
+++ app/models/news.rb
@@ -1,5 +1,5 @@
 class News < ActiveRecord::Base
-  named_scope :latest, lambda { |*args| {:conditions => ["publication_date > ?", (args.first || 2.weeks.ago)]} }
+  named_scope :latest, lambda { |*args| {:conditions => ["publication_date BETWEEN ? AND ?", (args[0]||2.weeks.ago).to_s(:short_format), (args[1]||Time.now).to_s(:short_format)]} }
 
   protected
   validates_presence_of :publication_date
