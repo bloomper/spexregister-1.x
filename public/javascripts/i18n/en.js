9fce619c3c73025e32f94a34aab80e565eadd8bc
Anders Jacobsson <anders.jacobsson@gmail.com>
2009-10-26 13:22:29 +0000

    Replaced jQuery Lightbox with FancyBox, simpler...

--- public/javascripts/i18n/en.js
+++ public/javascripts/i18n/en.js
@@ -1,22 +1,6 @@
 // English
 jQuery(function() {
-   /*
-   // jQuery Lightbox
-   jQuery('a[@rel*=lightbox]').lightBox({
-      imageLoading:     '/images/jquery.lightbox/lightbox-ico-loading.gif',
-      imageBtnPrev:     '/images/jquery.lightbox/lightbox-btn-prev_en.gif',
-      imageBtnNext:     '/images/jquery.lightbox/lightbox-btn-next_en.gif',
-      imageBtnClose:    '/images/jquery.lightbox/lightbox-btn-close_en.gif',
-      imageBlank:       '/images/jquery.lightbox/lightbox-blank.gif',
-      txtImage:         'Image',
-      txtOf:            'of',
-      keyToClose:       'c',
-      keyToPrev:        'p',
-      keyToNext:        'n'
-   });
-   */
-
-    // jQuery Alerts
+   // jQuery Alerts
    jQuery.alerts.okButton = '&nbsp;OK&nbsp;';
    jQuery.alerts.cancelButton = '&nbsp;Cancel&nbsp;';
 
