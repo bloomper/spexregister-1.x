d4dd08d4f4482501bfb46f856a2a93d752894b03
Anders Jacobsson <anders.jacobsson@gmail.com>
2009-10-31 08:56:55 +0000

    Mistake

--- app/models/function_activity.rb
+++ app/models/function_activity.rb
@@ -1,6 +1,6 @@
 class FunctionActivity < ActiveRecord::Base
   belongs_to :activity
   belongs_to :function 
-  has_one :actor, :dependent => :delete, :conditions => "function.category.has_actor = true"
+  has_one :actor, :dependent => :delete, :conditions => ["function.function_category.has_actor = ?", true]
 
 end
