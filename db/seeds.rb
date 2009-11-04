af6cb92b3cccfe6127ef78512ff436ca9af7c667
Anders Jacobsson <anders.jacobsson@gmail.com>
2009-11-04 20:06:27 +0000

    Simplified

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,10 @@
 #   Major.create(:name => 'Daley', :city => cities.first)
 
 # Users
-user = User.new :username => 'admin@chalmersspexet.se', :password => 'admin99', :password_confirmation => 'admin99', :user_groups => [ UserGroup.find_by_name('Administrators') ]
+user = User.new :username => 'admin@chalmersspexet.se', :password => 'admin99', :password_confirmation => 'admin99', :user_groups => [ UserGroup.find_by_name('Administrators') ], :state => 'active'
 user.save(false)
-user.approve!
-user = User.new :username => 'user@chalmersspexet.se', :password => 'user99', :password_confirmation => 'user99', :user_groups => [ UserGroup.find_by_name('Users') ]
+user = User.new :username => 'user@chalmersspexet.se', :password => 'user99', :password_confirmation => 'user99', :user_groups => [ UserGroup.find_by_name('Users') ], :state => 'active'
 user.save(false)
-user.approve!
 
 # Function categories
 FunctionCategory.create :name => 'Kommitté'
