edfc6bd3c0b10ee8e2f4d7ca1daeddf6d2a9d039
Anders Jacobsson <anders.jacobsson@gmail.com>
2009-11-05 20:38:41 +0000

    Added helpers for FGV and CING membership years

---
+++ app/helpers/memberships_helper.rb
@@ -0,0 +1,11 @@
+module MembershipsHelper
+
+  def get_fgv_years
+    Membership.get_fgv_years
+  end
+
+  def get_cing_years
+    Membership.get_cing_years
+  end
+
+end
--- app/models/membership.rb
+++ app/models/membership.rb
@@ -8,6 +8,28 @@ class Membership < ActiveRecord::Base
   named_scope :cing_memberships, :conditions => {:kind_id => 2}
   named_scope :by_kind, proc {|kind| { :conditions => { :kind_id => kind } } }
 
+  def self.get_fgv_years
+    Rails.cache.fetch('fgv_years') { (ApplicationConfig.first_fgv_year..Time.now.strftime('%Y').to_i).entries }
+  end
+  
+  def self.update_fgv_years
+    if self.get_fgv_years.max < Time.now.strftime('%Y').to_i
+      Rails.cache.delete('fgv_years')
+      self.get_fgv_years
+    end
+  end
+
+  def self.get_cing_years
+    Rails.cache.fetch('cing_years') { (ApplicationConfig.first_cing_year..Time.now.strftime('%Y').to_i).entries }
+  end
+  
+  def self.update_cing_years
+    if self.get_cing_years.max < Time.now.strftime('%Y').to_i
+      Rails.cache.delete('cing_years')
+      self.get_cing_years
+    end
+  end
+
   protected
   validates_inclusion_of_enum :kind_id, { :message => :inclusion, :allow_blank => false }
   validates_presence_of :year
