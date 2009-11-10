ced90d1a8a0dfee2954dd0daea3847ba4ec9d388
Anders Jacobsson <anders.jacobsson@gmail.com>
2009-11-10 12:49:51 +0000

    Simplified

--- app/helpers/function_categories_helper.rb
+++ app/helpers/function_categories_helper.rb
@@ -5,7 +5,7 @@ module FunctionCategoriesHelper
   end
 
   def get_available_function_categories_first_empty
-    function_categories = Array.new FunctionCategory.to_dropdown
+    function_categories = get_available_function_categories
     function_categories.insert(0, ['',''])
   end
 
--- app/helpers/spex_categories_helper.rb
+++ app/helpers/spex_categories_helper.rb
@@ -5,7 +5,7 @@ module SpexCategoriesHelper
   end
 
   def get_available_spex_categories_first_empty
-    spex_categories = Array.new SpexCategory.to_dropdown
+    spex_categories = get_available_spex_categories
     spex_categories.insert(0, ['',''])
   end
 
