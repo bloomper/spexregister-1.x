b6f60f10aae803ff471601714ef1235fc8ddfe6f
Anders Jacobsson <anders.jacobsson@gmail.com>
2009-11-18 11:02:27 +0000

    Fixed relationship part

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,28 +1,36 @@
 class RelationshipsController < ApplicationController
   inherit_resources
   actions :all, :except => [:update]
-  respond_to :html
+  respond_to :html, :except => [:create]
+  respond_to :js, :only => [:create]
   belongs_to :spexare, :singleton => true
-
+  
   def new
     new! do |format|
       format.html { render :layout => false }
     end
   end
-
+  
   def create
     @spexare = Spexare.find_by_id(params[:spexare_id])
     @spouse = Spexare.find_by_id(params[:spouse_id])
-    # TODO: Do some validation
-    Relationship.create(:spexare => @spexare, :spouse => @spouse)
-    @spexare.reload
-    render :action => 'edit', :layout => false
+    if @spouse
+      if !@spouse.spouse
+        Relationship.create(:spexare => @spexare, :spouse => @spouse)
+        @spexare.reload
+      else
+        flash.now[:error] = t 'flash.relationships.create.error'
+        return false
+      end
+    else 
+      flash.now[:error] = t 'flash.relationships.spexare_not_found'
+      return false
+    end
   end
-
+  
   def destroy
     @spexare = Spexare.find_by_id(params[:spexare_id])
     @spexare.relationship.destroy
-    render :action => 'edit', :layout => false
   end
-
+  
 end
