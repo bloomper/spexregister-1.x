827a6c6265ff3fd2d96ca4ac0e79a75cd686ab40
Anders Jacobsson <anders.jacobsson@gmail.com>
2009-11-20 09:33:40 +0000

    Started on activities

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,6 +1,36 @@
 class ActivitiesController < ApplicationController
   inherit_resources
+  actions :all, :except => [:show, :edit, :update]
   respond_to :html
   belongs_to :spexare
-  
+  before_filter :resource, :only => [:selected]
+
+  def new
+    new! do |format|
+      set_available_activities
+      format.html { render :layout => false }
+    end
+  end
+
+  def selected
+    @activities = @spexare.activities
+  end
+
+  protected
+  def resource
+    @activity ||= end_of_association_chain.find_by_id(params[:id])
+  end  
+
+  def collection
+    @activities ||= end_of_association_chain.find(:all)
+  end
+
+  private 
+  def set_available_activities
+     @available_memberships = Membership.get_years(kind.to_i).dup
+     @spexare.memberships.by_kind(kind).each do |membership|
+       @available_memberships.delete_if {|year| membership.year == year.to_s}
+     end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -30,7 +30,7 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :spexare do |spexare|
     spexare.resource :relationship, :except => [:update]
     spexare.resources :memberships, :except => [:show, :edit, :update], :collection => { :selected => :get }
-    spexare.resources :activities do |activity|
+    spexare.resources :activities, :except => [:show, :edit, :update], :collection => { :selected => :get } do |activity|
       activity.resource :spex_activity
       activity.resources :function_activities do |function_activity|
         function_activity.resource :actor
