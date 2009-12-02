cd56fcf3cf7e4ac7f7385bf6816aaf956b0c7b34
Anders Jacobsson <anders.jacobsson@gmail.com>
2009-12-02 21:04:51 +0000

    Checkpoint

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,13 +1,15 @@
 class SearchController < ApplicationController
-  
-  def new
-  end
-  
-  def index
-    @search = Spexare.search(params[:search])
+  inherit_resources
+  respond_to :html, :only => [:new, :index]
+  defaults :resource_class => Spexare, :collection_name => 'search_result', :route_collection_name => 'search_index', :route_instance_name => 'spexare'
+
+  protected
+  def collection
+    base_scope = end_of_association_chain
+    @search = base_scope.search(params[:search])
     @search.order ||= "ascend_by_last_name"
     
     @search_result ||= @search.paginate(:page => params[:page], :per_page => ApplicationConfig.entities_per_page)
   end
-  
+
 end
