d413d3d70b82126c903303b979fadb0060c36024
Anders Jacobsson <anders.jacobsson@gmail.com>
2009-12-07 21:49:22 +0000

    Checkpoint

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,17 +1,11 @@
 class ProfilesController < ApplicationController
   inherit_resources
-  respond_to :html, :only => [:edit, :show]
+  respond_to :html, :only => :edit
   defaults :resource_class => Spexare, :instance_name => 'spexare'
 
   def edit
     edit! do |format|
-      format.html { render 'spexare/edit', :locals => {:edit_mode => true, :current_action => t('views.base.editing_action'), :current_tab => 'spexare_tab'} }
-    end
-  end
-
-  def show
-    show! do |format|
-      format.html { render 'spexare/show', :locals => {:edit_mode => false, :current_action => t('views.base.viewing_action'), :current_tab => 'spexare_tab'} }
+      format.html { redirect_to edit_spexare_url(@spexare) }
     end
   end
 
