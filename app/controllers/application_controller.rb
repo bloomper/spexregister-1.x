0d30bbcc2bf2e19a3eb872f0863201d542360345
Anders Jacobsson <anders.jacobsson@gmail.com>
2009-12-08 19:42:08 +0000

    Added error handling for non-existent pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,17 +6,19 @@ class ApplicationController < ActionController::Base
   include SslRequirement
   helper :all # include all helpers, all the time
   helper_method :current_user_session, :current_user, :logged_in?, :current_user_is_admin?
-
+  
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
   
   # Scrub sensitive parameters from log
   filter_parameter_logging :password, :password_confirmation
   
+  rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found
+  
   before_filter :set_locale
   before_filter :check_session
   
   ssl_required
-
+  
   protected
   def set_locale
     locale = session[:locale] || I18n.default_locale
@@ -36,12 +38,17 @@ class ApplicationController < ActionController::Base
   def default_url_options(options={})
     { :locale => I18n.locale }
   end
-
+  
   def clear_authlogic_session
     sess = current_user_session
     sess.destroy if sess
   end
-
+  
+  def record_not_found
+    flash[:warning] = t 'views.base.record_not_found'
+    redirect_to home_path
+  end
+  
   private
   def current_user_session
     if defined?(@current_user_session) && !@current_user_session.nil?
