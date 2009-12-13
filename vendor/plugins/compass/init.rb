4d125d23a87794bf7601c71c2070c72586ad73a5
Anders Jacobsson <anders.jacobsson@gmail.com>
2009-12-13 21:07:52 +0000

    Avoid loading if 'rake gems:install' is being invoked

--- vendor/plugins/compass/init.rb
+++ vendor/plugins/compass/init.rb
@@ -1,2 +1,4 @@
-# This is here to make sure that the right version of sass gets loaded (haml 2.2) by the compass requires.
-require 'compass'
+if !(File.basename($0) == 'rake' && ARGV.include?('gems:install'))
+  # This is here to make sure that the right version of sass gets loaded (haml 2.2) by the compass requires.
+  require 'compass'
+end
--- vendor/plugins/jasperserver-client/init.rb
+++ vendor/plugins/jasperserver-client/init.rb
@@ -1,5 +1,7 @@
-# This file makes it possible to install JasperServer-Client as a Rails plugin.
-
-$: << File.expand_path(File.dirname(__FILE__))+'/lib'
-
-require 'jasperserver-client'
+if !(File.basename($0) == 'rake' && ARGV.include?('gems:install'))
+  # This file makes it possible to install JasperServer-Client as a Rails plugin.
+  
+  $: << File.expand_path(File.dirname(__FILE__))+'/lib'
+  
+  require 'jasperserver-client'
+end
