7a504683c9daefdb1fcd668eb924f3632096c1ac
Anders Jacobsson <anders.jacobsson@gmail.com>
2009-12-14 22:48:43 +0000

    Enable callbacks

--- config/deploy.rb
+++ config/deploy.rb
@@ -60,5 +60,5 @@ namespace :deploy do
 
 end
 
-#after  'deploy:symlink', 'deploy:asset:packager:build_all'
-#before 'deploy:asset:packager:build_all', 'deploy:sass:update'
+after  'deploy:symlink', 'deploy:asset:packager:build_all'
+before 'deploy:asset:packager:build_all', 'deploy:sass:update'
