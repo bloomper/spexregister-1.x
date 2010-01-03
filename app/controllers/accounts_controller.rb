e092c0a0bc65cdbe9c7fb77cf8f8ec533858d7cc
Anders Jacobsson <anders.jacobsson@gmail.com>
2010-01-03 22:08:57 +0000

    Oops, forgot to save forcefully

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -10,6 +10,7 @@ class AccountsController < ApplicationController
       @user = User.new :username => @captcha.values[:username], :password => @captcha.values[:password], :password_confirmation => @captcha.values[:password_confirmation], :user_groups => [ UserGroup.find_by_name('Users') ]
       @user.save
       if @user.errors.size == 1 && !@user.errors.on(:spexare).nil? && !@captcha.values[:full_name].blank? && !@captcha.values[:description].blank?
+        @user.save(false)
         AdminMailer.deliver_new_account_instructions(@captcha.values[:full_name], @captcha.values[:username], @captcha.values[:description])
         flash[:success] = I18n.t('flash.accounts.create.success')
         redirect_to login_path
