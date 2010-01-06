b202f8b5efc4cd27ca0a7342f8df3a03e4018c3e
Anders Jacobsson <anders.jacobsson@gmail.com>
2010-01-06 21:14:54 +0000

    Updated to latest inherited_resources

---
+++ config/initializers/responders.rb
@@ -0,0 +1,8 @@
+Responders::FlashResponder.flash_keys = [ :success, :failure ]
+
+class InheritedResources::Responder
+  def to_js
+    set_flash_message! if set_flash_message?
+    default_render
+  end
+end
