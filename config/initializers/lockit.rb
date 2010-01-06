7a2a52cfd0cad01ab3b640ef11d1815c16e4485f
Anders Jacobsson <anders.jacobsson@gmail.com>
2010-01-06 21:40:30 +0000

    Minimize startup time when sending mails

--- config/initializers/lockit.rb
+++ config/initializers/lockit.rb
@@ -1,4 +1,4 @@
-if !(File.basename($0) == 'rake' && ARGV.include?('db:migrate'))
+if !(File.basename($0) == 'rake' && ARGV.include?('db:migrate')) && !(File.basename($0) == 'runner')
   require 'lockdown'
   Lockdown.logger = Rails.logger
 end
