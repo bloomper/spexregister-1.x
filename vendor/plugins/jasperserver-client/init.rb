38bdfbd25962782b2a9479cd41a331c5fa3ae4d1
Anders Jacobsson <anders.jacobsson@gmail.com>
2010-02-09 18:57:41 +0000

    Temporarily removing jasperserver-client

--- vendor/plugins/jasperserver-client/init.rb
+++
@@ -1,7 +0,0 @@
-if !(File.basename($0) == 'rake' && ARGV.include?('gems:install'))
-  # This file makes it possible to install JasperServer-Client as a Rails plugin.
-  
-  $: << File.expand_path(File.dirname(__FILE__))+'/lib'
-  
-  require 'jasperserver-client'
-end
--- vendor/plugins/jasperserver-client/lib/jasper_server/error.rb
+++
@@ -1,4 +0,0 @@
-module JasperServer
-  class Error < Exception
-  end
-end
