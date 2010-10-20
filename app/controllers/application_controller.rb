4af6d0884c97351db993347ea52ea1ac301ee183
Anders Jacobsson <anders.jacobsson@gmail.com>
2010-10-20 22:16:54 +0000

    Updated to latest ExceptionNotification

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
-  include ExceptionNotifiable
+  include ExceptionNotification::Notifiable
   helper :all # include all helpers, all the time
   helper_method :current_user_session, :current_user, :logged_in?, :current_user_is_admin?, :current_protocol, :filter_url_if_not_compatible_with, :show_search_result_back_links?, :previous_page, :current_page, :latest_search_query_exists?, :latest_search_query, :latest_advanced_search_query_exists?, :latest_advanced_search_query, :get_available_dashboard_reports
   
--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -1,3 +1,3 @@
-ExceptionNotifier.exception_recipients = %w(register@chalmersspexet.se)
-ExceptionNotifier.sender_address = %("Application error" <register@chalmersspexet.se>)
+ExceptionNotification::Notifier.exception_recipients = %w(register@chalmersspexet.se)
+ExceptionNotification::Notifier.sender_address = %("Application error" <register@chalmersspexet.se>)
 
