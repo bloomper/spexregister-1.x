bfcda2547d8bae677fae5199d493bf7c5569db2c
Anders Jacobsson <anders.jacobsson@gmail.com>
2010-10-21 18:17:58 +0000

    Removed belongs_to_enum

--- vendor/plugins/belongs_to_enum/lib/belongs_to_enum.rb
+++
@@ -1,5 +0,0 @@
-# Declare your require's here
-require 'belongs_to_enum/object'
-require 'belongs_to_enum/acts_as_enum_field'
-require 'belongs_to_enum/enum_field'
-require 'belongs_to_enum/belongs_to_enum'
--- vendor/plugins/belongs_to_enum/lib/belongs_to_enum/object.rb
+++
@@ -1,8 +0,0 @@
-class Object
-  # adds methods to the metaclass
-  def meta_def name, &blk
-    metaclass.instance_eval do
-      define_method name, &blk
-    end
-  end
-end
