1548db82be3d5d13434026dc28d37d1db3681326
Anders Jacobsson <anders.jacobsson@gmail.com>
2011-01-27 22:14:44 +0000

    Added MIME types for all possible export formats (except XML which is built-in)

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -3,3 +3,12 @@
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
 # Mime::Type.register_alias "text/html", :iphone
+
+Mime::Type.register "application/pdf", :pdf
+Mime::Type.register "application/rtf", :rtf
+Mime::Type.register "application/vnd.oasis.opendocument.text", :odt
+Mime::Type.register "application/vnd.openxmlformats-officedocument.wordprocessingml.document", :docx
+Mime::Type.register "application/vnd.ms-excel", :xls
+Mime::Type.register "application/vnd.oasis.opendocument.spreadsheet", :ods
+Mime::Type.register "text/csv", :csv
+
