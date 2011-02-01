34d8f582c177de4e9ece4d248c6b55d7ac69b62b
Anders Jacobsson <anders.jacobsson@gmail.com>
2011-02-01 20:51:55 +0000

    Fixed sort problem of selected functions when configuring pluton list report ordering

--- app/helpers/settings_helper.rb
+++ app/helpers/settings_helper.rb
@@ -5,7 +5,15 @@ module SettingsHelper
   end
   
   def get_selected_functions(ids)
-    ids.blank? ? [] : Function.find(ids.split(',').collect{ |s| s.to_i })
+    ids.blank? ? [] : Function.find(ids.split(',').collect{ |s| s.to_i }, :order => "#{order_by_case('id', ids)}")
+  end
+
+  def order_by_case(column, ids)
+    expr = "case #{column}"
+    ids.split(',').each_with_index do |id, index|
+      expr += " when #{id} then #{index} "
+    end
+    expr += 'end'
   end
 
 end
