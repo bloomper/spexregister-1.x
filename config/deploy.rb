1a5b785b546cd18c994fe4ffc065880f6a622520
Anders Jacobsson <anders.jacobsson@gmail.com>
2011-05-16 17:27:02 +0000

    Cleanup

--- config/deploy.rb
+++ config/deploy.rb
@@ -78,24 +78,3 @@ end
 
 after 'deploy:symlink', 'deploy:asset:packager:build_all'
 before 'deploy:asset:packager:build_all', 'deploy:sass:update'
-
-namespace :delayed_job do
-  desc "Start delayed_job process"
-  task :start, :roles => :app do
-    run "cd #{current_path} && #{try_sudo} script/delayed_job start -- #{stage}"
-  end
-
-  desc "Stop delayed_job process"
-  task :stop, :roles => :app do
-    run "cd #{current_path} && #{try_sudo} script/delayed_job stop -- #{stage}"
-  end
-
-  desc "Restart delayed_job process"
-  task :restart, :roles => :app do
-    run "cd #{current_path} && #{try_sudo} script/delayed_job restart -- #{stage}"
-  end
-end
-
-after 'deploy:start', 'delayed_job:start'
-after 'deploy:stop', 'delayed_job:stop'
-after 'deploy:restart', 'delayed_job:restart'
