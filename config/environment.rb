0f3a149207448ba10e88df50d9e5a2a10b60adda
Anders Jacobsson <anders.jacobsson@gmail.com>
2011-06-16 20:01:45 +0000

    Merged changes from 1.1 maintenance

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.11' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.12' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -16,22 +16,23 @@ Rails::Initializer.run do |config|
   config.autoload_paths += %W( #{RAILS_ROOT}/app/mailers #{RAILS_ROOT}/app/utils #{RAILS_ROOT}/app/dashboard_reports #{RAILS_ROOT}/app/reports)
 
   # Specify gems that this application depends on and have them installed with rake gems:install
-  config.gem "sunspot_rails", :version => '1.2.1'
-  config.gem "erubis", :version => '2.7.0'
-  config.gem "haml", :version => '3.1.1'
-  config.gem "compass", :version => '0.11.1'
-  config.gem "hpricot", :version => '0.8.4'
-  config.gem "htmlentities", :version => '4.3.0'
-  config.gem "mysql", :version => '2.8.1'
-  config.gem "piston", :version => '2.0.10'
-  config.gem "rcov", :version => '0.8.1.2.0'
-  config.gem "encryptor", :version => '1.1.3'
-  config.gem "eigenclass", :version => '1.1.1'
-  config.gem "faraday", :version => '0.7.0'
-  config.gem "sqlite3-ruby", :lib => "sqlite3", :version => '1.3.3'
-  config.gem "xml-simple", :lib => 'xmlsimple', :version => '1.0.15'
-  config.gem "libxml-ruby", :lib => 'libxml', :version => '2.0.5'
+  config.gem 'sunspot_rails', :version => '1.2.1'
+  config.gem 'erubis', :version => '2.7.0'
+  config.gem 'haml', :version => '3.1.2'
+  config.gem 'compass', :version => '0.11.1'
+  config.gem 'hpricot', :version => '0.8.4'
+  config.gem 'htmlentities', :version => '4.3.0'
+  config.gem 'mysql', :version => '2.8.1'
+  config.gem 'piston', :version => '2.0.10'
+  config.gem 'rcov', :version => '0.8.1.2.0'
+  config.gem 'encryptor', :version => '1.1.3'
+  config.gem 'eigenclass', :version => '1.1.1'
+  config.gem 'faraday', :version => '0.7.0'
+  config.gem 'sqlite3-ruby', :lib => 'sqlite3', :version => '1.3.3'
+  config.gem 'xml-simple', :lib => 'xmlsimple', :version => '1.0.15'
+  config.gem 'libxml-ruby', :lib => 'libxml', :version => '2.0.5'
   config.gem 'BlueCloth', :lib => 'bluecloth', :version => '1.0.1'
+  config.gem 'rdoc'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
--- config/initializers/default_settings.rb
+++ config/initializers/default_settings.rb
@@ -1,5 +1,7 @@
 Settings.defaults['general.site_url'] = 'register.fgv.nu'
 Settings.defaults['general.admin_email_address'] = 'register.fgv.nu'
+Settings.defaults['general.become_fgv_member_url'] = 'https://www.fgv.nu/index.php?option=com_content&view=article&id=3&Itemid=6'
+Settings.defaults['general.fgv_address_update_email_address'] = 'adressuppdatering@fgv.nu'
 Settings.defaults['spex_categories.first_year'] = 1900
 Settings.defaults['reports.generator_url'] = 'http://localhost:8080/spexregister.reports'
 Settings.defaults['advanced_search.search_engine_url'] = 'http://localhost:8080/spexregister.search/all/'
